4c720c8c978a744de7fc9701ea3fb91a511f0355
Portia Antwi <portiaantwi257@gmail.com>
2022-06-13 16:18:18 -0700

    commit tree d9f99ae68e1e41ae44265a2094e7f607439ab925

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(Broom){
+    const appendCatCopy =[...cats, "Broom"];
+    return appendCatCopy;
+
+}
+function prependCat(Arnold){
+    const PrependCatCopy = ["Arnold", ...cats];
+    return PrependCatCopy;
+    
+}
+function removeLastCat(){
+    const slicedLast =cats.slice(0,2);
+    return slicedLast;
+}
+function removeFirstCat(){
+    const slicedFirst = cats.slice(1);
+    return slicedFirst;
+}
